492b89be0abc3df7189927916bcc429211a65b8e
Anthony <anthonyharisson30@gmail.com>
2022-07-13 15:00:31 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import imagem from "./img/sla.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+  state = {
+    nome: 'Anthony',
+    idade: '21',
+    comida: 'strogonoff',
+    musicas: ['I took a pill in ibiza', 'Slow Down', 'Rise up']
+
+  }
+  
+
+  render(){
+    return(
+      <div>
+        <h1>Fala pessoal, ma chamo {this.state.nome}!</h1>
+        <h2>Tenho {this.state.idade} anos!</h2>
+        <h3>Minha comida predileta é {this.state.comida}!</h3>
+        <h4>Minhas musicas prediletas estão abaixo!</h4>
+        <ol>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li>{this.state.musicas[2]}</li>
+        </ol>
+
+        <img src={imagem} alt='imagem'/>
+      </div>
+    )
+  }
 }
-
-export default App;
